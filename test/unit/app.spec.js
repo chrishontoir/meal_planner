6a050365b117563587d63ca4d37ef8490758a14c
chrishontoir <chrishontoir@gmail.com>
2020-10-10 21:14:57 +0100

    fix: updated app unit tests and added test linting

--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -1,36 +1,61 @@
 const td = require('testdouble');
 const assert = require('assert');
 
-describe('app.js', () => {
-    beforeEach(() => {
-        console.log = message => this.log = message;
+describe('app.js', function () {
+  beforeEach(function () {
+    console.log = message => { this.log = message; };
 
-        this.app = { listen: td.function() };
-        td.when(this.app.listen("3000")).thenCallback();
+    this.app = {
+      listen: td.function(),
+      use: td.function()
+    };
+    td.when(this.app.listen('3000')).thenCallback();
 
-        this.koa = td.function();
-        td.replace('koa', this.koa);
-        td.when(this.koa()).thenReturn(this.app);
+    this.koa = td.function();
+    td.replace('koa', this.koa);
+    td.when(this.koa()).thenReturn(this.app);
 
-        this.middleware = { logger: td.function() };
-        td.replace('../../src/middleware', this.middleware);
+    this.middleware = {
+      elapsedTime: td.function(),
+      logger: td.function(),
+      responseLogger: td.function()
+    };
+    td.replace('../../src/middleware', this.middleware);
 
-        this.config = { port: "3000" };
-        td.replace('../../src/config', this.config);
+    this.config = {
+      port: '3000'
+    };
+    td.replace('../../src/config', this.config);
 
-        this.sut = require('../../src/app');
-    });
-    afterEach(td.reset);
-    
-    it('should create a new Koa app on port 3000', () => {
-        td.verify(this.app.listen("3000", td.matchers.anything()));
-    });
+    this.router = {
+      routes: td.function(),
+      allowedMethods: td.function()
+    };
+    td.replace('../../src/router', this.router);
 
-    it('should set up the logger', () => {
-        td.verify(this.middleware.logger(this.app));
-    });
+    this.sut = require('../../src/app');
+  });
+  afterEach(td.reset);
 
-    it('should console log that the server has started', async () => {
-        assert.deepStrictEqual(this.log, 'Server started on port 3000');
-    });
+  it('should create a new Koa app on port 3000', function () {
+    td.verify(this.app.listen('3000', td.matchers.anything()));
+  });
+
+  it('should set up the logger', function () {
+    td.verify(this.middleware.logger(this.app));
+  });
+
+  it('should set up the middleware', function () {
+    td.verify(this.app.use(this.middleware.elapsedTime));
+    td.verify(this.app.use(this.middleware.responseLogger));
+  });
+
+  it('should set up the router', function () {
+    td.verify(this.app.use(this.router.routes()));
+    td.verify(this.app.use(this.router.allowedMethods()));
+  });
+
+  it('should console log that the server has started', async function () {
+    assert.deepStrictEqual(this.log, 'Server started on port 3000');
+  });
 });
