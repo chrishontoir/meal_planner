d2dccf8aaa14a56d560fb9e541028de9258e4e4b
chrishontoir <chrishontoir@gmail.com>
2020-10-10 21:33:36 +0100

    feat: added unit tests for config

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,5 @@
+console.log('A', process.env.NODE_ENV)
+
 const config = ['dev', 'test'].includes(process.env.NODE_ENV)
   ? require('./config.dev.json')
   : require('./config.prod.json');
---
+++ test/unit/config/index.spec.js
@@ -0,0 +1,64 @@
+const assert = require('assert');
+const decache = require('decache');
+
+describe('config / index.js', () => {
+    before(() => {
+        process.env.NODE_ENV = undefined;
+    })
+    afterEach(() => {
+        decache('../../../src/config');
+    })
+    after(() => {
+        process.env.NODE_ENV = 'test';
+    })
+
+    describe('when NODE_ENV is dev', () => {
+        beforeEach(() => {
+            process.env.NODE_ENV = 'dev';
+            this.sut = require('../../../src/config');
+        })
+        it('should load the dev config', () => {
+            assert.deepStrictEqual(this.sut, { port: "3000" })
+        })
+    })
+
+    describe('when NODE_ENV is test', () => {
+        beforeEach(() => {
+            process.env.NODE_ENV = 'test';
+            this.sut = require('../../../src/config');
+        })
+        it('should load the dev config', () => {
+            assert.deepStrictEqual(this.sut, { port: "3000" })
+        })
+    })
+
+    describe('when NODE_ENV is prod', () => {
+        beforeEach(() => {
+            process.env.NODE_ENV = 'prod';
+            this.sut = require('../../../src/config');
+        })
+        it('should load the prod config', () => {
+            assert.deepStrictEqual(this.sut, { port: "&&MEAL_PLANNER_API_PORT&&" })
+        })
+    })
+
+    describe('when NODE_ENV is undefined', () => {
+        beforeEach(() => {
+            process.env.NODE_ENV = undefined;
+            this.sut = require('../../../src/config');
+        })
+        it('should load the prod config', () => {
+            assert.deepStrictEqual(this.sut, { port: "&&MEAL_PLANNER_API_PORT&&" })
+        })
+    })
+
+    describe('when NODE_ENV is anything else', () => {
+        beforeEach(() => {
+            process.env.NODE_ENV = 'abcdefg';
+            this.sut = require('../../../src/config');
+        })
+        it('should load the prod config', () => {
+            assert.deepStrictEqual(this.sut, { port: "&&MEAL_PLANNER_API_PORT&&" })
+        })
+    })
+})
